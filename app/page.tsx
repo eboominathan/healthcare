d19b7b65cc0385a7a742b3b156f2843c31ad6c7f
Boominathan <eboominathan@gmail.com>
2024-07-06 11:04:01 +0530

    basic setup next-theme  completed

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+      <div>
+        <p className="text-3xl underline text-white">Home</p>
+          <Button>Click  me</Button>        
+      </div>
+  );
+}
